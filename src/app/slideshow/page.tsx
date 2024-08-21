c347044666011bfe0d03858ca3f58acf7902f521
QuintansC <gustavosantos.2095@gmail.com>
2024-08-21 14:39:42 -0300

    feat: initial commit

--- src/app/slideshow/page.tsx
+++ src/app/slideshow/page.tsx
@@ -1,9 +1,10 @@
+
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     hello world
+      teste slideshow
     </main>
   );
 }
