30ace12084db4fef1eb90ce1fb76d45deab4e791
QuintansC <gustavosantos.2095@gmail.com>
2024-08-22 11:28:15 -0300

    fix: sitemap languages

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,20 +5,46 @@ export default function sitemap(): MetadataRoute.Sitemap {
         {
             url: 'https://test-gustavo-quintans-git-master-quintanscs-projects.vercel.app/',
             lastModified: new Date(),
-            changeFrequency: 'yearly',
+            changeFrequency: 'weekly',
             priority: 1,
+            alternates: {
+                languages: {
+                    en: 'https://acme.com/',
+                },
+            },
         },
         {
             url: 'https://test-gustavo-quintans-git-master-quintanscs-projects.vercel.app/about',
             lastModified: new Date(),
-            changeFrequency: 'yearly',
+            changeFrequency: 'weekly',
             priority: 0.8,
+            alternates: {
+                languages: {
+                    en: 'https://acme.com/about',
+                },
+            },
         },
         {
-            url: 'https://test-gustavo-quintans-git-master-quintanscs-projects.vercel.app/blog',
+            url: 'https://test-gustavo-quintans-git-master-quintanscs-projects.vercel.app/slideshow',
             lastModified: new Date(),
-            changeFrequency: 'yearly',
+            changeFrequency: 'weekly',
             priority: 0.5,
+            alternates: {
+                languages: {
+                    en: 'https://acme.com/slideshow',
+                },
+            },
+        },
+        {
+            url: 'https://test-gustavo-quintans-git-master-quintanscs-projects.vercel.app/timeline',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+            alternates: {
+                languages: {
+                    en: 'https://acme.com/timeline',
+                },
+            },
         },
     ]
 }
