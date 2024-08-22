4fdc89a11f3e91826481f36f91abd9faa40a1927
QuintansC <gustavosantos.2095@gmail.com>
2024-08-22 11:22:39 -0300

    fix: correct images svg and create utils

---
+++ src/components/utils.ts
@@ -0,0 +1,9 @@
+import HomeIcon from "/public/icons8-home.svg";
+import AboutIcon from "/public/icons8-about.svg";
+import TimelineIcon from "/public/timeline.png";
+import SlideshowIcon from "/public/slideshow.png";
+import BackIcon from "/public/BackIcon.svg";
+import SwitcherLight from "/public/SwitcherLight.svg";
+import SwitcherDark from "/public/SwitcherDark.svg";
+
+export { HomeIcon, AboutIcon, TimelineIcon, SlideshowIcon, BackIcon, SwitcherLight, SwitcherDark }
